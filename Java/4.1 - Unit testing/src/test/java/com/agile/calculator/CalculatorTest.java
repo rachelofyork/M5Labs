d5e40cac6d08045e5b96d139a7c30548bd8cddef
r.hollingworth_wh <rachel.hollingworth@williamhill.co.uk>
2023-10-10 14:50:01 +0100

    added basic tests for calculator

--- Java/4.1 - Unit testing/src/test/java/com/agile/calculator/CalculatorTest.java
+++ Java/4.1 - Unit testing/src/test/java/com/agile/calculator/CalculatorTest.java
@@ -1,12 +1,34 @@
 package com.agile.calculator;
 
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculatorTest {
 
+	static Calculator calculator;
+
+	@BeforeClass
+	public static void setup(){
+		calculator = new Calculator();
+	}
+
 	@Test
 	public void subtractionTest(){
-		fail("Not yet implemented..");
+		assertEquals(10, calculator.subtract("20,10"));
+	}
+	
+
+	@Test
+	public void multiplyTest(){
+		assertEquals(10, calculator.multiply("2,5"));
+	}
+
+	@Test
+	public void divideTest(){
+		assertEquals(2, calculator.divide("6,3"));
 	}
 }
